da2a889cd98677c5e074ebd3638f6c899b215c5a
AdamMomen <adammuman81@gmail.com>
2020-01-10 17:48:00 +0100

    merged changes

--- server/socket.js
+++ server/socket.js
@@ -1,17 +1,9 @@
 function handler(socket) {
-<<<<<<< HEAD
     socket.on('room', (room) => {
         socket.join(room)
     })
 
-    socket.on('message', (data) => {
-=======
-    socket.on('room', (room)=>{
-        socket.join(room)
-    })
-
-    socket.on('message', ({user, chatid, text})=>{
->>>>>>> c10ec224fa27ffaf66fac04fcfac04a4f3414e5a
+    socket.on('message', ({ user, chatid, text }) => {
         //save to DB
         //emit to all in room
     })
