6c0ec5c5d8d20aa5b2e262d201cad4ffc9aa500a
MohamedBelkheirRBK <mohamedbel@protonmail.com>
2020-01-10 20:35:45 +0100

    fixing more of malik's shitcode

--- controllers/Friends.js
+++ controllers/Friends.js
@@ -30,7 +30,7 @@ function Check(username) {
       if(data.rowCount<1) throw "no friends";
       var friends = data.rows.reduce((acc, row)=>{
         console.log(row)
-        row.friend1 === username  ? row.push(row.friend2) : acc.push(row.friend1);
+        row.friend1 === username? row.push(row.friend2) : acc.push(row.friend1);
         return acc;
       }, [])
       return friends
