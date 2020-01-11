a8f42f2222870d2448141087d751dc7976992634
MalikAltawati <Malik.abdullah.abraheem@gmail.com>
2020-01-11 16:07:14 +0100

    pending model documented

--- models/Pending.js
+++ models/Pending.js
@@ -8,16 +8,20 @@ con.query(PendingSchema, (err, data) => {
   if (err) console.error(err);
   else console.log("Pending Table IS UP");
 });
+//######################################################## Add friendship request into pending friendship requests table
 
 function SendFriendRequest(requester, target) {
   return con.query(
     `INSERT into pending(requester,target) VALUES ('${requester}', '${target}')`
   );
 }
+//######################################################## get all friend requests of a specific user 
 
 function CheckFriendRequest(username) {
   return con.query(`SELECT * FROM pending WHERE target = '${username}'`);
 }
+
+//######################################################## remove a friend request from the pending table 
 
 function DeleteFriendRequest(requester, target) {
   return con.query(
