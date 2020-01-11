7d04445c5e9a22c7b413cbe873a07b1646dcc3b1
AdamMomen <adammuman81@gmail.com>
2020-01-11 11:00:22 +0100

    friends render works

--- controllers/Pending.js
+++ controllers/Pending.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-const PendingModel = require("../models/Pending");
-/*
-module.exports.SendFriendRequest = SendFriendRequest;
-module.exports.CheckFriendRequest = CheckFriendRequest;
-module.exports.DeleteFriendRequest = DeleteFriendRequest;
- */
-function Send(requester, target) {
-  return PendingModel.SendFriendRequest(requester, target)
-    .then(data => {
-      return data;
-    })
-    .catch(err => {
-      throw err;
-    })
-}
-function Fetch(username) {
-  return PendingModel.CheckFriendRequest(username)
-    .then(data => {
-      console.log(data.rows)
-      var friendRequests = data.rows.map(row => {
-        return row.requester
-      })
-      return friendRequests;
-    })
-    .catch(err => {
-      throw err;
-    });
-}
-function DeleteFromPending(requester, target) {
-  return PendingModel.DeleteFriendRequest(requester, target)
-    .then(data => {
-      return data;
-    })
-    .catch(err => {
-      throw err;
-    });
-}
-
-module.exports.Send = Send;
-module.exports.Fetch = Fetch;
-module.exports.DeleteFromPending = DeleteFromPending;
-=======
 const PendingModel = require("../models/Pending");
 /*
 module.exports.SendFriendRequest = SendFriendRequest;
@@ -59,7 +16,7 @@ function Send(requester, target) {
 function Fetch(username) {
   return PendingModel.CheckFriendRequest(username)
     .then(data => {
-      return data.rows.map(row=>row.requester);
+      return data.rows.map(row => row.requester);
     })
     .catch(err => {
       throw err;
@@ -78,4 +35,3 @@ function DeleteFromPending(requester, target) {
 module.exports.Send = Send;
 module.exports.Fetch = Fetch;
 module.exports.DeleteFromPending = DeleteFromPending;
->>>>>>> 9c827a722aa4ce68b363e570ae218517302e62e4
