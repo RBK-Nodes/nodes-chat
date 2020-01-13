d52881bb1b8f0eeb0660bdc204d38750be8abe90
Malik Altawati <56149000+Malik-Altawati@users.noreply.github.com>
2020-01-13 17:28:03 +0100

    chatrooms controller documented

--- controllers/chatrooms.js
+++ controllers/chatrooms.js
@@ -1,5 +1,5 @@
 const ChatRoomsModel = require("../models/chatrooms");
-
+//######################################################## this takes  2 users and passes them to the function that creates a room for them in the chatrooms table
 function Create(user1, user2) {
   return ChatRoomsModel.CreateRoom(user1, user2)
     .then(data => {
@@ -9,6 +9,7 @@ function Create(user1, user2) {
       throw err;
     });
 }
+//######################################################## this takes 2 users and passes them to the function that returns their chatroom id
 function getroomId(user1, user2) {
   return ChatRoomsModel.findRoom(user1, user2)
     .then(data => {
