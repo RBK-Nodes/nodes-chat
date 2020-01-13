1b07bcf16af3a7c4f8f4ba739335a2b21c1d8f08
Malik Altawati <56149000+Malik-Altawati@users.noreply.github.com>
2020-01-13 17:24:39 +0100

    Pending controller documented

--- controllers/Pending.js
+++ controllers/Pending.js
@@ -1,9 +1,5 @@
 const PendingModel = require("../models/Pending");
-/*
-module.exports.SendFriendRequest = SendFriendRequest;
-module.exports.CheckFriendRequest = CheckFriendRequest;
-module.exports.DeleteFriendRequest = DeleteFriendRequest;
- */
+//######################################################## this takes  2 users and passes them to the function that addes them both and pending friend requests table
 function Send(requester, target) {
   return PendingModel.SendFriendRequest(requester, target)
     .then(data => {
@@ -13,6 +9,7 @@ function Send(requester, target) {
       throw err;
     });
 }
+//######################################################## this takes a username passes it to the function that returns all friend requests to a certain user 
 function Fetch(username) {
   return PendingModel.CheckFriendRequest(username)
     .then(data => {
@@ -22,6 +19,8 @@ function Fetch(username) {
       throw err;
     });
 }
+//######################################################## this takes a two users and passes them to a function that deletes them from pending table 
+
 function DeleteFromPending(requester, target) {
   return PendingModel.DeleteFriendRequest(requester, target)
     .then(data => {
