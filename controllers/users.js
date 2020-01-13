0933f0390ef8aec5c5a97f39aa636be68d421c31
Malik Altawati <56149000+Malik-Altawati@users.noreply.github.com>
2020-01-13 17:33:49 +0100

    users controller documented

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const usersModel = require("../models/users");
+//######################################################## this takes a username and passes it to the function that checks if user exists
 
 function finduser(username) {
   return usersModel
